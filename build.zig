3cd0099932fcf2fbd6c77267e7d33aab4a682191
smnaugle <smnaugle@sas.upenn.edu>
2026-01-09 15:32:22 -0800

    Improvements to build script and compiling standalone library

--- build.zig
+++ build.zig
@@ -1,17 +1,11 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     const nlopt_path = b.option([]const u8, "nlopt_install_dir", "Path where NLOPT is installed") orelse "";
     var nlopt_inc_path: []u8 = "";
-    defer arena.deinit();
-    const allocator = arena.allocator();
     const has_nlopt_path = !std.mem.eql(u8, nlopt_path, "");
     if (has_nlopt_path) {
-        nlopt_inc_path = std.mem.concat(allocator, u8, &.{ nlopt_path, "include/" }) catch |err| {
-            std.debug.panic("{}", .{err});
-            return;
-        };
+        nlopt_inc_path = b.fmt("{s}/include", .{nlopt_path});
         b.addSearchPrefix(nlopt_path);
     } else {
         std.log.warn("nlopt_install_dir build option is not set, assuming that the library is installed on default path", .{});
@@ -19,23 +13,33 @@ pub fn build(b: *std.Build) void {
 
     const target = std.Build.standardTargetOptions(b, .{});
     const optimize = std.Build.standardOptimizeOption(b, .{});
-    const mod = b.addModule("llfit", .{ .root_source_file = b.path("src/root.zig"), .target = target, .optimize = optimize });
-    mod.linkSystemLibrary("nlopt", .{});
+    const mod = b.addModule("llfit", .{
+        .link_libc = true,
+        .optimize = optimize,
+        .target = target,
+        .root_source_file = b.path("src/root.zig"),
+    });
+    const lib = b.addLibrary(.{
+        .name = "llfit",
+        .root_module = mod,
+    });
     if (has_nlopt_path) {
-        mod.addSystemIncludePath(.{ .cwd_relative = nlopt_inc_path });
+        lib.root_module.addSystemIncludePath(.{ .cwd_relative = nlopt_inc_path });
     }
+    lib.root_module.linkSystemLibrary("nlopt", .{});
+    b.installArtifact(lib);
     const exe = b.addExecutable(.{
         .name = "llfit_test",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
+            .link_libc = true,
         }),
     });
-    exe.linkLibC();
-    exe.root_module.addImport("llfit", mod);
-    b.installArtifact(exe);
+    exe.root_module.addImport("llfit", lib.root_module);
     if (has_nlopt_path) {
         exe.root_module.addSystemIncludePath(.{ .cwd_relative = nlopt_inc_path });
     }
+    b.installArtifact(exe);
 }
